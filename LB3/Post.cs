093cecb5dcd2feee781de0e296f14228e8d6cd5e
Бадретдинов Кирилл <raynor730@mail.ru>
2024-12-12 09:46:07 +0500

    Добавление покетов,Генерация контекста данных из существующей БД.

---
+++ LB3/Post.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LB3;
+
+public partial class Post
+{
+    public int Id { get; set; }
+
+    public int IdUser { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Context { get; set; } = null!;
+
+    public DateTime DareOfPublication { get; set; }
+
+    public virtual User IdUserNavigation { get; set; } = null!;
+}
