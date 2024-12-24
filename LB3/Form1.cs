ba6806f375882fda989881bc9ae4abf4c9e08d2b
Бадретдинов Кирилл <raynor730@mail.ru>
2024-12-24 12:55:27 +0500

    Изменение схемы БД, отображение таблиц из базы данных в DataGridView

--- LB3/Form1.cs
+++ LB3/Form1.cs
@@ -1,10 +1,64 @@
+
+using LB3.Models;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+
 namespace LB3
 {
-    public partial class Form1 : Form
+    public partial class FormMain : Form
     {
-        public Form1()
+        // Создание экземпляра контекста данных, который будет использываться
+        // для загрузки и отслеживания изменений о пользователях
+        private BlogContext? db;
+        public FormMain()
         {
             InitializeComponent();
+        }
+
+        // Метод OnLoad вызывается при загрузке формы
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            this.db = new BlogContext();
+
+            // метод Load расширения используется для загрузки всех пользователей
+            // из базы данных в BlogContext базу данных
+            this.db.Users.Load();
+            this.dataGridViewUsers.DataSource = db.Users.Local.ToBindingList();
+        }
+
+        // Метод OnClosing вызывается при закрытии формы
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+
+            this.db?.Dispose();
+            this.db = null;
+        }
+
+        private void DataGridViewUsers_SelectionChanged(object sender, EventArgs e)
+        {
+            if (this.db != null)
+            {
+                var user = (User)this.dataGridViewUsers.CurrentRow.DataBoundItem;
+
+                if (user != null)
+                {
+                    this.db.Entry(user).Collection(e => e.Posts).Load();
+                    this.dataGridViewPosts.DataSource = user.Posts;
+                }
+            }
+        }
+
+        private void ButtonSave_Click(object sender, EventArgs e)
+        {
+            // Этот код вызывает SaveChanges DbContext объект 
+            // который сохраняет все изменения, внесенные в базу данных
+            this.db!.SaveChanges();
+
+            this.dataGridViewUsers.Refresh();
+            this.dataGridViewPosts.Refresh();
+            
         }
     }
 }
